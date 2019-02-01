388a18ec85eea79bf910dafb11ddc23f74bd808f
nikitabutkevich <44376155+nikitabutkevich@users.noreply.github.com>
2019-02-01 20:03:56 +0300

    js object hw

---
+++ js_object/scriptObject.js
@@ -0,0 +1,94 @@
+//Task 1
+console.log('Задание №1');
+
+var city = {
+	location: 'Europe',
+	name: 'Paris',
+	contry: 'France',
+	population: 12220000
+}
+
+var stringPrompt = 'population';
+
+function findStringInObject(city, stringPrompt){
+	if (stringPrompt in city) return true;
+	return false;
+}
+
+console.log(findStringInObject(city, stringPrompt));
+console.log(' ');
+
+
+//Task 2
+console.log('Задание №2');
+
+var recipe = {
+	name: 'Драники',
+
+	ingredients: {
+		potato: 1,
+		onion: 1/8,
+		flour: 1/8,
+		salt: 1/8,
+		pepper: 1/8
+	},
+
+	portion: 1
+}
+
+var getPortion = +prompt('Введите количество порций', '');
+
+function getRecipe(recipe, getPortion) {
+	if (getPortion >= recipe.portion ) {
+		console.log('Готовим ' + recipe.name + '. На ' + getPortion + ' порц(ию, ий, ии) нам понадобиться:');
+		console.log('	Картошка: ' + recipe.ingredients.potato * getPortion + ' шт.');
+		console.log('	Лук: ' + recipe.ingredients.onion * getPortion + ' шт.');
+		console.log('	Мука: ' + recipe.ingredients.flour * getPortion + ' ст. л.');
+		console.log('	Соль: ' + recipe.ingredients.salt * getPortion + ' щепотки');
+		console.log('	Перец: ' + recipe.ingredients.pepper * getPortion + ' щепотки');
+	} else return 'Не может быть меньше одной порции';
+}
+
+console.log(getRecipe(recipe, getPortion));
+console.log(' ');
+
+
+//Task 3
+console.log('Задание №3');
+
+var cylinder = {
+	radius: 3,
+	height: 5,
+	volume: function() {
+		console.log('Обьем цилиндра равен = ' + 3.14 * (this.radius ** 2) * this.height);
+	}
+}
+
+console.log(cylinder.volume());
+console.log(' ');
+
+
+//Task 4
+console.log('Задание №4');
+
+var penguin = {
+	name: 'Whiteblack',
+	author: 'Margret Rey',
+	origin: 'Whiteblack the Penguin Sees the World',
+	canFly: false,
+
+	sayHello: function(){
+		console.log('Привет, дружок! Меня зовут ' + this.name + ' :)');
+	},
+
+	fly: function(){
+		console.log('Я ' + this.name + ' и Я могу летаааать');
+	}
+}
+
+function getPenguin(penguin){
+	if (penguin.canFly == false) console.log(penguin.sayHello());
+	else console.log(penguin.fly());
+}
+
+console.log(getPenguin(penguin));
